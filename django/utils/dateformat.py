5221c595f57a931cf0699ebfa3be2bbe58297018
Sriram Magesh <srirammagesh11april2008@gmail.com>
2026-07-02 12:14:53 +0300

    Fix timezone handling in DateFormat r() method

--- django/utils/dateformat.py
+++ django/utils/dateformat.py
@@ -270,7 +270,10 @@ class DateFormat(TimeFormat):
         if not isinstance(value, datetime):
             # Assume midnight in default timezone if datetime.date provided.
             default_timezone = get_default_timezone()
-            value = datetime.combine(value, time.min).replace(tzinfo=default_timezone)
+            value = make_aware(
+                datetime.combine(value, time.min),
+                timezone=default_timezone,
+            )
         elif is_naive(value):
             value = make_aware(value, timezone=self.timezone)
         return format_datetime_rfc5322(value)
